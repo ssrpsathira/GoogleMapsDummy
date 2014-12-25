863a532f13212ffa4ae55fa1716059094b36709f
ssrp <ssrpsathira@gmail.com>
2014-12-25 20:56:08 +0530

    add district regions

--- downloadLocations.php
+++ downloadLocations.php
@@ -7,6 +7,7 @@ set_time_limit(0);
  $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+//  curl_setopt($ch, CURLOPT_PROXY, "cache.mrt.ac.lk:3128");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                      'Content-Type: application/json',
