d3d95507f88d2f75efb4b3735be020b337bc2717
ssrp <ssrpsathira@gmail.com>
2015-01-10 01:38:18 +0530

    complete show location latest statistics feature

---
+++ downloadLatestLocationStatistics.php
@@ -0,0 +1,23 @@
+<?php
+
+set_time_limit(0);
+$data = array('metadata' => array('service' => 'noise', 'mode' => 'download', 'feature' => 'latestLocationStatistics'), 'rawdata' => array('location_id' => $_GET['locationId']));
+$data_string = json_encode($data);
+$url = 'http://localhost/CDME/request.php';
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+//  curl_setopt($ch, CURLOPT_PROXY, "cache.mrt.ac.lk:3128");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+
+//$json_result = json_decode($result, true);
+?>
