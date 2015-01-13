7ceb18dfdcee127de34c5f9a1fd8c55b1ad40413
ssrp <ssrpsathira@gmail.com>
2015-01-13 17:12:32 +0530

    complete district statistic graphs

--- downloadKmzLayer.php
+++ downloadKmzLayer.php
@@ -1,7 +1,7 @@
 <?php
 
 set_time_limit(0);
-$data = array('metadata' => array('service' => 'noise', 'mode' => 'download', 'feature' => 'adminRegionStatistics'), 'rawdata' => array('region_id' => $_GET['region_id'], 'region_level' => $_GET['region_level'], 'from_date' => $_GET['from_date'], 'to_date' => $_GET['to_date'], 'sampling_rate' => $_GET['sampling_rate']));
+$data = array('metadata' => array('service' => 'noise', 'mode' => 'download', 'feature' => 'admin2kmzLayer', 'imei' => $_GET['imei']), 'rawdata' => array('from_date' => $_GET['from_date'], 'to_date' => $_GET['to_date'], 'sampling_rate' => $_GET['sampling_rate']));
 $data_string = json_encode($data);
 $url = 'http://156.56.93.34/CDME/request.php';
 
