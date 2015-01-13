d55935cd683242cf9de1d696f9ed2b90cdf4864f
ssrp <ssrpsathira@gmail.com>
2015-01-13 21:44:50 +0530

    complete features

--- downloadLatestLocationStatistics.php
+++ downloadLatestLocationStatistics.php
@@ -1,7 +1,7 @@
 <?php
 
 set_time_limit(0);
-$data = array('metadata' => array('service' => 'noise', 'mode' => 'download', 'feature' => 'latestLocationStatistics'), 'rawdata' => array('location_id' => $_GET['locationId']));
+$data = array('metadata' => array('service' => 'noise', 'mode' => 'download', 'feature' => 'latestLocationStatistics'), 'rawdata' => array('location_id' => $_GET['locationId'], 'from_date' => $_GET['from_date'], 'to_date' => $_GET['to_date'], 'displacement_range' => $_GET['displacement_range']));
 $data_string = json_encode($data);
 $url = 'http://156.56.93.34/CDME/request.php';
 
